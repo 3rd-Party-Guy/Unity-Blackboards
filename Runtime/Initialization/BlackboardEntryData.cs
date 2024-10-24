3c2fd5b098d6e6b5d4762e6f87c3855b41d68be5
Nikolay Hadzhiev <nikolay.hadzhiev@therabytes.de>
2024-10-24 10:59:27 +0200

    Add unity types to dispatch table

--- Runtime/Initialization/BlackboardEntryData.cs
+++ Runtime/Initialization/BlackboardEntryData.cs
@@ -17,13 +17,15 @@ namespace ThirdPartyGuy.Collections
             setValueDispatchTable[value.type](blackboard, key, value);
         }
 
-        Dictionary<AnyValue.Type, Action<Blackboard, Key, AnyValue>> setValueDispatchTable = new()
+        readonly Dictionary<AnyValue.Type, Action<Blackboard, Key, AnyValue>> setValueDispatchTable = new()
         {
             { AnyValue.Type.Bool, (blackboard, key, value) => blackboard.Set<bool>(key, value) },
             { AnyValue.Type.Int, (blackboard, key, value) => blackboard.Set<int>(key, value) },
             { AnyValue.Type.Float, (blackboard, key, value) => blackboard.Set<float>(key, value) },
             { AnyValue.Type.String, (blackboard, key, value) => blackboard.Set<int>(key, value) },
             { AnyValue.Type.Vector3, (blackboard, key, value) => blackboard.Set<Vector3>(key, value) },
+            { AnyValue.Type.Rigidbody, (blackboard, key, value) => blackboard.Set<Rigidbody>(key, value) },
+            { AnyValue.Type.GameObject, (blackboard, key, value) => blackboard.Set<GameObject>(key, value) },
         };
 
         public void OnBeforeSerialize() { }
